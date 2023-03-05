2659db8e3b082493e57cff2c61a84776a03be044
Pedro Rubinger <pedro.rubinger@gmail.com>
2023-03-05 00:28:54 -0300

    refactor: move ProfileCard to Home page instead of using it in Cover layout component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,8 @@
 import React from "react"
 
-import { HomeContainer } from "~/pages/Home/styles"
+import { CoverLayoutProfileContainer, HomeContainer } from "~/pages/Home/styles"
+import GenericAvatar from "~/assets/GenericAvatar.svg"
+import { ProfileCard } from "~/components/ProfileCard"
 import { SearchPosts } from "~/components/SearchPosts"
 
 interface HomeProps {}
@@ -8,6 +10,17 @@ interface HomeProps {}
 export const Home: React.FC<HomeProps> = () => {
 	return (
 		<HomeContainer>
+			<CoverLayoutProfileContainer>
+				<ProfileCard
+					followers={8}
+					avatar={GenericAvatar}
+					name="Pedro Rubinger"
+					username="pedrorubinger"
+					company="Luby Software"
+					url="https://github.com/pedrorubinger"
+					bio="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse cum numquam nemo nisi quia eos expedita, commodi qui autem et officiis vel assumenda dolor libero quos dolorum, consequatur nihil quod."
+				/>
+			</CoverLayoutProfileContainer>
 			<SearchPosts amount={6} />
 		</HomeContainer>
 	)
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,3 +1,15 @@
 import styled from "styled-components"
 
 export const HomeContainer = styled.div``
+
+export const CoverLayoutProfileContainer = styled.div`
+	width: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	margin-top: -85px;
+
+	@media (max-width: 900px) {
+		margin-top: -2px;
+	}
+`
