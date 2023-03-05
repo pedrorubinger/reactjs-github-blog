dbe6340d268087e254185488fb792050523e752f
Pedro Rubinger <pedro.rubinger@gmail.com>
2023-03-05 00:24:18 -0300

    refactor: replace div with styled container in Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,14 @@
 import React from "react"
 
+import { HomeContainer } from "~/pages/Home/styles"
 import { SearchPosts } from "~/components/SearchPosts"
 
 interface HomeProps {}
 
 export const Home: React.FC<HomeProps> = () => {
 	return (
-		<div>
+		<HomeContainer>
 			<SearchPosts amount={6} />
-		</div>
+		</HomeContainer>
 	)
 }
