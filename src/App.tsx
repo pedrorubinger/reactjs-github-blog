6a86d2dad829144bff120ad84fdb1c20a35a7085
Pedro Rubinger <pedro.rubinger@gmail.com>
2023-03-04 23:40:26 -0300

    feat: apply Router component in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { ThemeProvider } from "styled-components"
+import { BrowserRouter } from "react-router-dom"
 
 import { defaultTheme } from "~/themes/Default"
 import { GlobalStyles } from "~/themes/Global"
+import { Router } from "~/Router"
 
 const App = () => {
 	return (
 		<ThemeProvider theme={defaultTheme}>
+			<BrowserRouter>
+				<Router />
+			</BrowserRouter>
+
 			<GlobalStyles />
 		</ThemeProvider>
 	)
