be17a19aa4377399085a457856602fa302c17f1f
Pedro Rubinger <pedro.rubinger@gmail.com>
2023-03-06 18:46:17 -0300

    feat: add GetGitHubProfileData interface

---
+++ src/interfaces/GitHubProfileData.ts
@@ -0,0 +1,12 @@
+export interface GitHubProfileData {
+	login: string // username
+	id: number
+	html_url: string
+	node_id: string
+	avatar_url: string
+	name: string
+	company: string
+	followers: number
+	bio: string
+	location: string
+}
