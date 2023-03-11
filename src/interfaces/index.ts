e876318a08a2bfa5157cee6dbf46b786c67d6e72
Pedro Rubinger <pedro.rubinger@gmail.com>
2023-03-11 15:20:34 -0300

    feat: add new interfaces in the index files of interfaces directory

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,3 +1,10 @@
 export * from "~/interfaces/Post"
+
 export * from "~/interfaces/GetProfile"
+export * from "~/interfaces/GetRepoIssue"
+export * from "~/interfaces/SearchRepoIssue"
+
+export * from "~/interfaces/GitHubEndpoint"
 export * from "~/interfaces/GitHubProfileData"
+export * from "~/interfaces/GitHubRepoIssueData"
+export * from "~/interfaces/GitHubSearchRepoIssue"
