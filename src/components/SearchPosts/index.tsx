408a73fc386c5a8b46084f609739bdb3d368d2b4
Pedro Rubinger <pedro.rubinger@gmail.com>
2023-03-11 15:34:18 -0300

    feat: search issues from API of a defined GitHub repository

--- src/components/SearchPosts/index.tsx
+++ src/components/SearchPosts/index.tsx
@@ -1,4 +1,10 @@
-import React from "react"
+import React, {
+	useCallback,
+	useContext,
+	useEffect,
+	useRef,
+	useState,
+} from "react"
 
 import {
 	SearchPostsAmount,
@@ -6,25 +12,58 @@ import {
 	SearchPostsHeader,
 	SearchPostsTitle,
 } from "~/components/SearchPosts/styles"
+import { PostContext } from "~/contexts"
 import { Input } from "~/components/Input"
 
-interface SearchPostsProps {
-	amount: number
-}
+interface SearchPostsProps {}
 
-export const SearchPosts: React.FC<SearchPostsProps> = ({ amount = 0 }) => {
-	const postsAmountLabel = amount === 1 ? "publicação" : "publicações"
+export const SearchPosts: React.FC<SearchPostsProps> = () => {
+	const timeout = useRef<NodeJS.Timeout | undefined>()
+	const { posts, fetchIssues } = useContext(PostContext)
+
+	const [search, setSearch] = useState<string>("")
+
+	const amount = posts.length
+	const amountLabel = amount === 1 ? "publicação" : "publicações"
+
+	const onChangeText = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setSearch(e.target.value)
+	}
+
+	const searchIssues = useCallback(
+		async (value: string) => {
+			await fetchIssues(value)
+		},
+		[fetchIssues]
+	)
+
+	useEffect(() => {
+		if (search) {
+			timeout.current = setTimeout(() => {
+				void searchIssues(search)
+			}, 1200)
+		}
+
+		return () => {
+			clearTimeout(timeout.current)
+		}
+	}, [searchIssues, search])
 
 	return (
 		<SearchPostsContainer>
 			<SearchPostsHeader>
 				<SearchPostsTitle>Publicações</SearchPostsTitle>
 				<SearchPostsAmount>
-					{amount} {postsAmountLabel}
+					{amount} {amountLabel}
 				</SearchPostsAmount>
 			</SearchPostsHeader>
 
-			<Input placeholder="Buscar conteúdo" type="text" />
+			<Input
+				placeholder="Buscar conteúdo"
+				type="text"
+				value={search}
+				onChange={onChangeText}
+			/>
 		</SearchPostsContainer>
 	)
 }
