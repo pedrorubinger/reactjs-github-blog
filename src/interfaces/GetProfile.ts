bf1cb84184585c7a404d31bed859957c9994d46e
Pedro Rubinger <pedro.rubinger@gmail.com>
2023-03-11 08:46:28 -0300

    chore: remove unused interface GetUserProfileProps

--- src/interfaces/GetProfile.ts
+++ src/interfaces/GetProfile.ts
@@ -1,9 +1,5 @@
 import { GitHubProfileData } from "~/interfaces/GitHubProfileData"
 
-export interface GetUserProfileProps {
-	username: string
-}
-
 export interface GetUserProfileResponse {
 	success: boolean
 	data: GitHubProfileData | null
